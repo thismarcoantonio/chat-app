87629248e4764b26616904a55bd14855273899aa
thismarcoantonio <thismarcoantonio@gmail.com>
2022-06-22 20:54:21 -0300

    Add more rooms to events

--- server/src/events.ts
+++ server/src/events.ts
@@ -11,6 +11,36 @@ const rooms: Room[] = [
     name: "General",
     private: false,
   },
+  {
+    id: "2",
+    maxUsers: 10,
+    name: "Games",
+    private: false,
+  },
+  {
+    id: "3",
+    maxUsers: 2,
+    name: "Pair-Programming",
+    private: false,
+  },
+  {
+    id: "4",
+    maxUsers: 20,
+    name: "Jibber jabber",
+    private: false,
+  },
+  {
+    id: "5",
+    maxUsers: 15,
+    name: "Investment",
+    private: false,
+  },
+  {
+    id: "6",
+    maxUsers: 10,
+    name: "Reading books",
+    private: false,
+  },
 ];
 
 const users: User[] = [];
