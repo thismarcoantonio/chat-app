71cb885e8b979dc4c2ca0ae53ffcb26a08593d60
thismarcoantonio <thismarcoantonio@gmail.com>
2022-06-25 20:46:39 -0300

    Add responsive to home page

--- client/src/components/logo.ts
+++ client/src/components/logo.ts
@@ -2,7 +2,7 @@ import { html } from "lit-html";
 
 export function logo() {
   return html`
-    <h1 class="text-2xl	text-white flex items-center">
+    <h1 class="inline-flex text-2xl	text-white items-center">
       CHAT
       <span
         class="text-lg bg-white text-gray-900 font-bold pl-2 pr-6 py-1 ml-2 rounded-sm"
--- client/src/pages/home.ts
+++ client/src/pages/home.ts
@@ -10,8 +10,12 @@ interface HomeParams {
 
 export function homePage({ rooms }: HomeParams) {
   return html`
-    <div class="flex items-center min-h-screen pb-20 justify-between px-20">
-      <div class="max-w-sm shrink-0 mr-24">
+    <div
+      class="mx-auto px-6 pt-10 min-h-screen max-w-screen-lg md:justify-between md:flex md:items-center md:pb-20 md:px-20"
+    >
+      <div
+        class="text-center md:text-left md:max-w-sm md:shrink-0 md:mr-14 lg:mr-24"
+      >
         ${logo()}
         <p class="mt-2 text-slate-200 text-white">
           Chat with your friends without thinking much, go ahead and join in!
@@ -27,21 +31,30 @@ export function homePage({ rooms }: HomeParams) {
           </button>
         </form>
       </div>
-      <ul id="rooms-list" class="w-full max-w-sm bg-gray-800 p-4 rounded-lg max-h-[65vh] overflow-auto">
-        ${rooms.map((room) => html`
-          <li class="flex items-center justify-between bg-white text-left px-4 py-3 rounded-lg shadow-lg mb-4 last:mb-0">
-            <div>
-              <p class="font-bold mb-1 text-slate-800">${room.name}</p>
-              <div class="flex items-center text-sm text-slate-600">
-                <div class="w-1.5 h-1.5 bg-lime-600 rounded-full mr-2"></div>
-                5 online now (${room.maxUsers} max)
+      <ul
+        id="rooms-list"
+        class="my-14 mx-auto w-full max-w-sm bg-gray-800 p-4 rounded-lg md:mx-0 md:max-h-[65vh] md:overflow-auto"
+      >
+        ${rooms.map(
+          (room) => html`
+            <li
+              class="flex items-center justify-between bg-white text-left px-4 py-3 rounded-lg shadow-lg mb-4 last:mb-0"
+            >
+              <div>
+                <p class="font-bold mb-1 text-slate-800">${room.name}</p>
+                <div class="flex items-center text-sm text-slate-600">
+                  <div class="w-1.5 h-1.5 bg-lime-600 rounded-full mr-2"></div>
+                  5 online now (${room.maxUsers} max)
+                </div>
               </div>
-            </div>
-            <button class="text-slate-800 flex items-center pl-2 pr-1 py-1 rounded transition-colors hover:bg-slate-200">
-              Join ${arrowRightIcon()}
-            </button>
-          </li>
-        `)}
+              <button
+                class="text-slate-800 flex items-center pl-2 pr-1 py-1 rounded transition-colors hover:bg-slate-200"
+              >
+                Join ${arrowRightIcon()}
+              </button>
+            </li>
+          `
+        )}
       </ul>
     </div>
   `;
